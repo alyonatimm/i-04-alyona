a922787d2019ab7304bfe41507045e13ed37328a
Alyona <al2004@mail.ru>
2022-10-17 05:34:21 +0000

    creat new repository

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+	$a = 4;
+	$b = 2;
+	$c = -1;
+	$D = $b*$b - 4*$a*$c;
+	if ($D > 0){
+		$x1 = (-$b + sqrt($D)) / (2 * $a);
+		$x2 = (-$b - sqrt($D)) / (2 * $a);
+		echo 'x1 = '.$x1.'<br>x2 = '.$x2;
+	}
+	else if ($D == 0) {
+		$x = -$b / 2*$a;
+		echo 'x = '.$x;
+	}
+	else {
+		echo 'No sqrt';
+	}
+?>
